a046dd2a8647a5ee69e440230b3eb6cd714870d7
JespervB2005 <jespervanbenthem@ziggo.nl>
2024-03-12 10:46:00 +0100

    Silent Orchestra Damage tijdens piep toevoegen

--- LC_Addtions/Patches/Silent_Orchestra.cs
+++ LC_Addtions/Patches/Silent_Orchestra.cs
@@ -15,22 +15,43 @@ namespace LC_Addtions.Patches
     {
 
         public AudioSource audioSource;
+        private float audioTimer = 0;
+        private float damageTimer = 0;
 
         void Awake()
         {
             audioSource = GetComponent<AudioSource>();
+            audioSource.volume = 0.5f;
 
         }
 
-        public override void ItemActivate(bool used, bool buttonDown = true)
+        public override void LateUpdate()
         {
-            base.ItemActivate(used, buttonDown);
-            if (buttonDown)
+            base.LateUpdate();
+            if (audioTimer > 0)
             {
-                
-                audioSource.PlayOneShot(LC_Addtions.SoundFX[0]);
-
+                audioTimer -= Time.deltaTime;
+                damageTimer -= Time.deltaTime;
             }
+            if (audioTimer > 37 && audioTimer < 49.5) 
+            { 
+                if (playerHeldBy != null && damageTimer <= 0) 
+                {
+                    playerHeldBy.DamagePlayer(10);
+                    damageTimer = 0.5f;
+                }
+            }
+        }
+
+        public override void GrabItem()
+        {
+            base.GrabItem();
+            if (audioTimer <= 0 ) 
+            {
+                audioSource.PlayOneShot(LC_Addtions.SoundFX[0]);
+                audioTimer = 90;
+            }
+
         }
     }
 }
